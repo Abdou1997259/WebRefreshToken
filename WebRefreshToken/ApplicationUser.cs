d529eb77974eaa43d0469c99c32a878f8c63495e
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-12-01 18:29:22 +0200

    Add project files.

---
+++ WebRefreshToken/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebRefreshToken
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
